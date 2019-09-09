015ed748477b9acddfec422c566f0572d48168a4
skumari1 <skumari1@in.ibm.com>
2019-09-09 22:21:08 +0530

    Adding lifecycle logs in all the fragment to track orientation change

--- app/src/main/java/com/example/fragmentlifecycledemo/fragments/FragmentC.kt
+++ app/src/main/java/com/example/fragmentlifecycledemo/fragments/FragmentC.kt
@@ -1,10 +1,12 @@
 package com.example.fragmentlifecycledemo.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
 import com.example.fragmentlifecycledemo.MainActivity
 import com.example.fragmentlifecycledemo.R
 import kotlinx.android.synthetic.main.fragment_c.*
@@ -32,6 +34,56 @@ class FragmentC : BaseFragment()
         buttonPop.setOnClickListener {
             (activity as MainActivity).pop()
         }
+    }
+
+    override fun onCreateAnimation(transit: Int, enter: Boolean, nextAnim: Int): Animation? {
+        Log.i(LOG_TAG, "onCreateAnimation")
+        return super.onCreateAnimation(transit, enter, nextAnim)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.i(LOG_TAG, "onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(LOG_TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(LOG_TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(LOG_TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(LOG_TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(LOG_TAG, "onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i(LOG_TAG, "onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(LOG_TAG, "onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i(LOG_TAG, "onDetach")
     }
 
 }
